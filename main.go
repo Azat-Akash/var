fcbd1e9cc50bda79f18b39bf0a694d0f9ee522bb
Azat-Akash <azat.akash@nu.edu.kz>
2024-03-07 12:17:05 +0500

    вфвфвф

--- main.go
+++ main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"math"
+	"math/big"
+	"os"
+	"strings"
 )
 
 func main() {
 
-	i := 0.0
-	fmt.Scan(&i)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	val := strings.Split(scanner.Text(), " ")
+	num1 := new(big.Int)
+	num2 := new(big.Int)
+	num3 := new(big.Int)
 
-	if float64(int(math.Log2(i))) == math.Log2(i) {
-		fmt.Println("YES")
-	} else {
-		fmt.Println("NO")
+	_, success1 := num1.SetString(val[0], 10)
+	_, success2 := num2.SetString(val[1], 10)
+	_, success3 := num3.SetString(val[2], 10)
+
+	if !success1 || !success2 || !success3 {
+		return
 	}
+
+	max := num1
+	if num2.Cmp(max) == 1 {
+		max = num2
+	}
+	if num3.Cmp(max) == 1 {
+		max = num3
+	}
+
+	fmt.Println(max)
+
 }
